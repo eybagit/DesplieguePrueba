e11df3e103708b0cf3ac8c58ba5fab2c63786621
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-10-01 19:12:53 -0500

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -5,6 +5,9 @@ set -o errexit
 npm install
 npm run build
 
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install psycopg2-binary==2.9.9
+pip install -r requirements.txt --no-deps
+pip install flask flask-sqlalchemy flask-migrate flask-cors gunicorn python-dotenv
 
 flask db upgrade
